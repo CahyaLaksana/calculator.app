c9da4a08c7ef88fbc2270199f2dff95ba2f31450
CahyaLaksana <cahya.laksana@students.amikom.ac.id>
2020-03-13 19:27:17 +0700

    pembaruan

--- calculator.cs
+++ calculator.cs
@@ -1,18 +1,24 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Calculator
     {
         static void Main(string[] args)
-        { Console.Title = "Apliaction Calculator";
-			// ini komentar saya
+        {
+            Console.Title = "Apliaction Calculator";
+            // ini komentar saya
             //int a = 10; //deklarasi nilai a, dengan nilai awal 10
             //int b = 6;  //deklarasi nilai b, dengan nilai awal 6
+            A:
+            Console.WriteLine("Pilihlah salah satu");
+            Console.WriteLine("1. Penambahan ");
+            Console.WriteLine("2. Pengurangan ");
+            Console.WriteLine("3. Perkalian ");
+            Console.WriteLine("4. Pembagian ");
+
+            Console.Write("\nNomor yang dipilih: ");
+            int f = int.Parse(Console.ReadLine());
 
             Console.Write("Inputkan Nilai a = ");
             int a = int.Parse(Console.ReadLine()); //proses casting
@@ -22,10 +28,24 @@ namespace Calculator
 
             Console.WriteLine(); //tambahkan baris kosong
 
-            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
-            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            if (f == 1){
+                Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            }else if (f == 2)
+            {
+                Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            }
+            else if (f == 3)
+            {
+                Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            }
+            else if (f == 4)
+            {
+                Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            }
+            else{ Console.WriteLine("\n\n Pilihan yang anda pilih tidak ada \"TOLONG ULANGI LAGI\"\n\n");
+                Console.ReadKey();
+                goto A;
+            }
 
             Console.WriteLine("\nTekan sembarang key untuk keluar");
             Console.ReadKey();
@@ -43,12 +63,12 @@ namespace Calculator
 
         static int Perkalian(int a, int b)
         {
-            return a* b;
+            return a * b;
         }
 
-        static int Pembagian(int a, int b)
+        static double Pembagian(double a, double b)
         {
             return a / b;
-        }   
+        }
     }
 }
